5e942e89cd8d05b7c90b9f93934c10f5d4cb028e
wpeterashworth <wingsaseagles12@gmail.com>
2024-01-23 18:35:13 -0800

    additional practice

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,4 +1,21 @@
-const myArray = document.createElement("Array");
-myArray.innerText = (["Hello", "World"]);
-document.body.appendChild(myArray);
-myArray.pop(last);
+// const myArray = document.createElement("Array");
+// myArray.innerText = (["Hello", "World"]);
+// document.body.appendChild(myArray);
+// myArray.pop(last);
+
+let firstName = "Antonia";
+let lastName = "Francesca";
+
+// function fullName(firstName, lastName) {
+//     return (`${firstName} ${lastName}`);
+// }
+
+// const fullName = function(firstName, lastName) {
+//     return (`${firstName} ${lastName}`);
+// }
+
+const fullName = (firstName, lastName) => {
+    return (`${firstName} ${lastName}`);
+}
+
+document.querySelector("#fullName").innerHTML = fullName(firstName, lastName);
