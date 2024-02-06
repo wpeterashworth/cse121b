85c8d0a3f9ef67bbef3958230ac67689926079ab
wpeterashworth <wingsaseagles12@gmail.com>
2024-02-06 13:25:38 -0800

    Finished modules prepare material

---
+++ scripts/modules.js
@@ -0,0 +1,12 @@
+import aCourse from "./Course.js"
+  
+  document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum);
+  });
+  document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum, false);
+  });
+
+  aCourse.init();
